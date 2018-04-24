35d98e4e3166abb33d32dc206e05372250c38fc5
LesiaVakhula <vakhulalesia@gmail.com>
2018-04-24 11:49:30 +0300

    fixed bug bootstrap, header,...

--- components/common/app.js
+++ components/common/app.js
@@ -1,13 +1,19 @@
 const angular = require('angular');
 import "./styles/style.scss";
 require('@uirouter/angularjs');
+import "bootstrap";
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+
+require('@uirouter/angularjs');
 const home = require('../main/emAppMain.js');
+const shopCard = require('../shopCard/shopCard.js');
 const eventPage = require('../main/eventModule.js')
 
 
-module.exports = angular.module('emApp', ['ui.router', home, eventPage])
+module.exports = angular.module('emApp', ['ui.router', home, shopCard, eventPage])
 .config([
 	'$locationProvider',function($locationProvider) {
   $locationProvider.html5Mode(true);
 }])
-.component('footerComponent', require('./utils-components/footer/footerComponent.js'));
+.component('footerComponent', require('./utils-components/footer/footerComponent.js'))
+    .component('headerComponent', require('./utils-components/header/headerComponent'));
