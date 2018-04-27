423ca76e6be2174c322c1b35010510bd24c17ee3
Stepan1992 <chopkostepan1992@gmail.com>
2018-04-27 14:17:45 +0300

    fixed bugs with borders

--- components/home/emAppHome.js
+++ components/home/emAppHome.js
@@ -3,43 +3,38 @@ const template = require('./homeTemplate.html');
 require('./homeStyle.scss');
 require('@uirouter/angularjs');
 
-
 module.exports = angular.module('emApp.home', ['ui.router'])
+  .config(['$stateProvider', function ($stateProvider) {
+    var homePageState = {
+      name: 'home',
+      url: '/',
+      templateUrl: template,
+      controller: (['$scope', function ($scope) {
+        $scope.classesArray = [{
+            name: 'birthday',
+            className: 'birthday',
+            imageUrl: 'birthday/homepage_birthday.png'
+          },
+          {
+            name: 'funeral',
+            className: 'funeral',
+            imageUrl: 'funeral/homepage_funeral.png'
+          },
+          {
+            name: 'conference',
+            className: 'conference',
+            imageUrl: 'conference/homepage_conference.png'
+          },
+          {
+            name: 'wedding',
+            className: 'wedding',
+            imageUrl: 'wedding/homepage_wedding.png'
+          }
+        ];
+      }])
+    };
 
-
-.config(['$stateProvider', function($stateProvider) {
-  var homePageState = {
-    name: 'home',
-    url:'/',
-    templateUrl : template,
-    controller: (['$scope', function($scope) {
-
-   $scope.classesArray = [
-    {
-    name: 'birthday',
-    className: 'birthday',
-    imageUrl: 'birthday/homepage_birthday.png'
-   },
-   {
-    name: 'funeral',
-    className: 'funeral',
-    imageUrl: 'funeral/homepage_funeral.png'
-   }, 
-    {
-    name: 'conference',
-    className: 'conference',
-    imageUrl: 'conference/homepage_conference.png'
-   },
-   {
-    name: 'wedding',
-    className: 'wedding',
-    imageUrl: 'wedding/homepage_wedding.png'
-   }
-   ];
-    }])
-  };
-
-  $stateProvider.state(homePageState);
-}])
-.component('eventActionComponent', require('../common/utils-components/eventActionComponent/eventActionComponent.js'))
-.name;
+    $stateProvider.state(homePageState);
+  }])
+  .component('eventActionComponent', require('../common/utils-components/eventActionComponent/eventActionComponent.js'))
+  .name;
