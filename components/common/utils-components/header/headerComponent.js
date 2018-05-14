8e0e0c9aa2aa61c65bebab15102b8be9a29bf040
Stepan1992 <chopkostepan1992@gmail.com>
2018-05-14 16:35:22 +0300

    add validation to registration form

--- components/common/utils-components/header/headerComponent.js
+++ components/common/utils-components/header/headerComponent.js
@@ -29,30 +29,34 @@ module.exports = {
 					'ngInject';
 
 					$scope.regError = false;
+					$scope.changePassForm = function () {
+						$scope.regError = false;
+					};
 
-					$scope.signInFunc = function (valid) {
-						$http.get('/getUser', {
-							params: {
-								email: $scope.userEmail,
-								pass: $scope.userPass
-							}
-						}).then(function successCallback(response) {
-							if (response.data) {
-								$scope.regError = false;
-								filterFactory.isUser = true;
-								filterFactory.userEmail = response.data.email;
-								$rootScope.$broadcast('sendUserLog', {
-									isUser: true,
-									userEmail: response.data.email
-								});
-
-								ngDialog.closeAll();
-							} else {
-								$scope.regError = true;
-							}
-						}, function errorCallback(response) {
-							console.log('Error!!!');
-						});
+					$scope.signInFunc = function (isValid) {
+						if (isValid) {
+							$http.get('/getUser', {
+								params: {
+									email: $scope.userEmail,
+									pass: $scope.userPass
+								}
+							}).then(function successCallback(response) {
+								if (response.data) {
+									$scope.regError = false;
+									filterFactory.isUser = true;
+									filterFactory.userEmail = response.data.email;
+									$rootScope.$broadcast('sendUserLog', {
+										isUser: true,
+										userEmail: response.data.email
+									});
+									ngDialog.closeAll();
+								} else {
+									$scope.regError = true;
+								}
+							}, function errorCallback(response) {
+								console.log('Error!!!');
+							});
+						};
 					};
 				}
 			});
@@ -64,22 +68,33 @@ module.exports = {
 				className: 'ngdialog-theme-default modal-view',
 				controller: function ($scope, $http) {
 					'ngInject';
-					$scope.signUpFunc = function () {
+					$scope.emailError = false;
+					$scope.changeRegEmail = function () {
+						$scope.emailError = false;
+					};
+					$scope.regExpPhone = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+					$scope.regExpName = /^[a-z ,.'-]+$/i;
+					$scope.signUpFunc = function (isValid) {
+						if (isValid) {
+							let registrationObj = {
+								firstName: $scope.firstName,
+								lastName: $scope.lastName,
+								phoneNumber: $scope.phoneNumber,
+								email: $scope.email,
+								password: $scope.pass
+							};
 
-						let registrationObj = {
-							firstName: $scope.firstName,
-							lastName: $scope.lastName,
-							phoneNumber: $scope.phoneNumber,
-							email: $scope.email,
-							password: $scope.pass
+							$http.post('/setNewUser', registrationObj)
+								.then(function successCallback(response) {
+									if (response.data.email) {
+										$scope.emailError = true;
+									} else {
+										ngDialog.closeAll();
+									};
+								}, function errorCallback(response) {
+									console.log('Error!!!');
+								});
 						};
-
-						$http.post('/setNewUser', registrationObj)
-							.then(function successCallback(response) {
-								ngDialog.closeAll();
-							}, function errorCallback(response) {
-								console.log('Error!!!');
-							});
 					};
 				}
 			});
